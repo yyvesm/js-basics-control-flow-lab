33065ac9f64be6c70e4c1ee96c1387832a853e2d
yves <yvesmomplais@hotmail.com>
2021-06-14 07:24:31 -0400

    commit tree 6bf289efea8d5176ccdc8761dbff4a152fdc983e

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!' }
+    else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'}
+    else if (someValue > 2500) {
+    return 'No can do.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ if (city === 'NYC') {
+ return 'Ok, sounds good.'}
+ else {
+ return 'No go.'}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+case 'generous':
+return 'Thank you so much.'
+case 'not as generous':
+return 'Thank you.'
+case 'thanks for everything':
+return 'Bye.'
+} 
 }
